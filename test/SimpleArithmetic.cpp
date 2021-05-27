ee93d404d98742d3c255f73ea8686f1ff20ea3ea
Jobin Johnson <jobinrjohnson@gmail.com>
2021-05-27 11:31:57 +0530

    test: unit test bare bones

---
+++ test/SimpleArithmetic.cpp
@@ -0,0 +1,37 @@
+//
+// Created by jobinrjohnson on 27/05/21.
+//
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <flare.h>
+
+int EXPECTED_EXIT_CODE = 2;
+
+std::string CODE = R"(
+
+function main(): int {
+    return 1 + 1;
+}
+
+)";
+
+int main() {
+
+    std::stringstream ss;
+    ss.str(CODE);
+
+    // Execute
+    flare::Flare f;
+    f.setInputStream(ss);
+    f.executeJit();
+
+    // Assert
+    if (f.getExitCode() != EXPECTED_EXIT_CODE) {
+        return 1;
+    }
+
+    return 0;
+
+}
