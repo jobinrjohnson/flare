74b54d9f16708a92ec4a93bc6a26d1c157c4599a
Jobin Johnson <jobinrjohnson@gmail.com>
2021-06-15 20:01:21 +0530

    sample: implement a sample get int function

--- samples/1.ts
+++ samples/1.ts
@@ -1,12 +1,54 @@
 function printf(formatString: int, params: int): void;
+function printf(formatString: int): void;
 
 function getchar(): int;
 
+class Console {
+
+    isNumericInputOk(ip: int): boolean {
+        if (ip < 48) {
+            return false;
+        }
+        if (ip > 57) {
+            return false;
+        }
+        return true;
+    }
+
+    getInt(): int {
+
+        let final = 0;
+        let isNegative = false;
+
+        let ip: int = getchar();
+
+        if (ip == 45) {
+            isNegative = true;
+            ip = getchar();
+        }
+
+        while (this.isNumericInputOk(ip)) {
+            final = final * 10 + (ip - 48);
+            ip = getchar();
+        }
+
+        if (isNegative) {
+            final = -final;
+        }
+
+        return final;
+
+    }
+
+}
+
+
 function main(): int {
 
-    printf("Enter the %s", "number.!");
+    printf("Enter the number : ");
 
-    let n: int = getchar() - 48;
+    let i = new Console();
+    let n: int = i.getInt();
 
     printf("The input is %d", n);
 
