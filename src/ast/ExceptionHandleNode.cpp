1a757e686266a4c33deafc4678812380cb16c4df
Jobin Johnson <jobinrjohnson@gmail.com>
2021-07-04 18:02:42 +0530

    exception: added llvm exception handling [3]

--- src/ast/ExceptionHandleNode.cpp
+++ src/ast/ExceptionHandleNode.cpp
@@ -24,9 +24,41 @@ namespace flare::ast {
         cxt->getCurrentFunction()->popExceptionHandler();
 
         auto currentBlk = cxt->getBuilder()->GetInsertBlock();
-
+        // Exception block
         builder.SetInsertPoint(exceptionBlock);
-        builder.CreateRet(llvm::ConstantInt::get(context, APInt(64, 1)));
+
+
+        auto extBlock = llvm::BasicBlock::Create(context,
+                                                 "external",
+                                                 cxt->getCurrentFunction()->getLLVMFunctionRef());
+
+
+        // Create LLVM struct type for exception
+        std::vector<llvm::Type *> items;
+        items.push_back(builder.getInt8PtrTy());
+        items.push_back(builder.getInt32Ty());
+        static llvm::StructType *exceptionCaughtType = llvm::StructType::get(context,
+                                                                             items);
+
+        llvm::LandingPadInst *caughtResult = builder.CreateLandingPad(
+                exceptionCaughtType,
+                1,
+                "exception_info"
+        );
+        caughtResult->setCleanup(true);
+
+        llvm::Value *caughtResultStorage = new AllocaInst(exceptionCaughtType, 0, "s",
+                                                          cxt->getBuilder()->GetInsertBlock());
+
+        builder.CreateStore(caughtResult, caughtResultStorage);
+        // TODO add catch handling cases
+
+        // Exit block in case of no handler
+        builder.CreateBr(extBlock);
+        builder.SetInsertPoint(extBlock);
+        builder.CreateResume(builder.CreateLoad(caughtResultStorage));
+
+
         builder.SetInsertPoint(currentBlk);
 
         return nullptr;
